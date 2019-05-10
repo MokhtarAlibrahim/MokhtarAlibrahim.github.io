9a2f4572b8327e3515b66515e058beb789d853f2
MokhtarAlibrahim <50489532+MokhtarAlibrahim@users.noreply.github.com>
2019-05-10 16:13:57 +0300

    Add files via upload

---
+++ js/index.js
@@ -0,0 +1,28 @@
+var data = 
+[{"Year":1880,"temrature":-0.19},{"Year":1881,"temrature":-0.11},{"Year":1882,"temrature":-0.12},{"Year":1883,"temrature":-0.2},{"Year":1884,"temrature":-0.3},{"Year":1885,"temrature":-0.32},{"Year":1886,"temrature":-0.33},{"Year":1887,"temrature":-0.36},{"Year":1888,"temrature":-0.18},{"Year":1889,"temrature":-0.11},{"Year":1890,"temrature":-0.37},{"Year":1891,"temrature":-0.24},{"Year":1892,"temrature":-0.27},{"Year":1893,"temrature":-0.32},{"Year":1894,"temrature":-0.32},{"Year":1895,"temrature":-0.23},{"Year":1896,"temrature":-0.12},{"Year":1897,"temrature":-0.12},{"Year":1898,"temrature":-0.28},{"Year":1899,"temrature":-0.19},{"Year":1900,"temrature":-0.09},{"Year":1901,"temrature":-0.16},{"Year":1902,"temrature":-0.3},{"Year":1903,"temrature":-0.39},{"Year":1904,"temrature":-0.49},{"Year":1905,"temrature":-0.28},{"Year":1906,"temrature":-0.23},{"Year":1907,"temrature":-0.4},{"Year":1908,"temrature":-0.44},{"Year":1909,"temrature":-0.48},{"Year":1910,"temrature":-0.43},{"Year":1911,"temrature":-0.43},{"Year":1912,"temrature":-0.36},{"Year":1913,"temrature":-0.36},{"Year":1914,"temrature":-0.16},{"Year":1915,"temrature":-0.12},{"Year":1916,"temrature":-0.33},{"Year":1917,"temrature":-0.44},{"Year":1918,"temrature":-0.28},{"Year":1919,"temrature":-0.27},{"Year":1920,"temrature":-0.26},{"Year":1921,"temrature":-0.18},{"Year":1922,"temrature":-0.27},{"Year":1923,"temrature":-0.25},{"Year":1924,"temrature":-0.25},{"Year":1925,"temrature":-0.21},{"Year":1926,"temrature":-0.09},{"Year":1927,"temrature":-0.21},{"Year":1928,"temrature":-0.19},{"Year":1929,"temrature":-0.35},{"Year":1930,"temrature":-0.15},{"Year":1931,"temrature":-0.1},{"Year":1932,"temrature":-0.17},{"Year":1933,"temrature":-0.3},{"Year":1934,"temrature":-0.14},{"Year":1935,"temrature":-0.21},{"Year":1936,"temrature":-0.16},{"Year":1937,"temrature":-0.04},{"Year":1938,"temrature":-0.04},{"Year":1939,"temrature":-0.03},{"Year":1940,"temrature":0.11},{"Year":1941,"temrature":0.18},{"Year":1942,"temrature":0.05},{"Year":1943,"temrature":0.07},{"Year":1944,"temrature":0.2},{"Year":1945,"temrature":0.08},{"Year":1946,"temrature":-0.07},{"Year":1947,"temrature":-0.04},{"Year":1948,"temrature":-0.11},{"Year":1949,"temrature":-0.11},{"Year":1950,"temrature":-0.18},{"Year":1951,"temrature":-0.07},{"Year":1952,"temrature":0.01},{"Year":1953,"temrature":0.07},{"Year":1954,"temrature":-0.15},{"Year":1955,"temrature":-0.14},{"Year":1956,"temrature":-0.21},{"Year":1957,"temrature":0.04},{"Year":1958,"temrature":0.07},{"Year":1959,"temrature":0.03},{"Year":1960,"temrature":-0.02},{"Year":1961,"temrature":0.05},{"Year":1962,"temrature":0.04},{"Year":1963,"temrature":0.07},{"Year":1964,"temrature":-0.2},{"Year":1965,"temrature":-0.1},{"Year":1966,"temrature":-0.05},{"Year":1967,"temrature":-0.02},{"Year":1968,"temrature":-0.07},{"Year":1969,"temrature":0.07},{"Year":1970,"temrature":0.03},{"Year":1971,"temrature":-0.09},{"Year":1972,"temrature":0.01},{"Year":1973,"temrature":0.16},{"Year":1974,"temrature":-0.08},{"Year":1975,"temrature":-0.02},{"Year":1976,"temrature":-0.11},{"Year":1977,"temrature":0.17},{"Year":1978,"temrature":0.06},{"Year":1979,"temrature":0.16},{"Year":1980,"temrature":0.27},{"Year":1981,"temrature":0.33},{"Year":1982,"temrature":0.13},{"Year":1983,"temrature":0.31},{"Year":1984,"temrature":0.16},{"Year":1985,"temrature":0.12},{"Year":1986,"temrature":0.18},{"Year":1987,"temrature":0.33},{"Year":1988,"temrature":0.41},{"Year":1989,"temrature":0.28},{"Year":1990,"temrature":0.44},{"Year":1991,"temrature":0.41},{"Year":1992,"temrature":0.22},{"Year":1993,"temrature":0.24},{"Year":1994,"temrature":0.31},{"Year":1995,"temrature":0.45},{"Year":1996,"temrature":0.34},{"Year":1997,"temrature":0.47},{"Year":1998,"temrature":0.62},{"Year":1999,"temrature":0.4},{"Year":2000,"temrature":0.4},{"Year":2001,"temrature":0.53},{"Year":2002,"temrature":0.62},{"Year":2003,"temrature":0.61},{"Year":2004,"temrature":0.53},{"Year":2005,"temrature":0.67},{"Year":2006,"temrature":0.62},{"Year":2007,"temrature":0.64},{"Year":2008,"temrature":0.52},{"Year":2009,"temrature":0.63},{"Year":2010,"temrature":0.7},{"Year":2011,"temrature":0.59},{"Year":2012,"temrature":0.62},{"Year":2013,"temrature":0.65},{"Year":2014,"temrature":0.73},{"Year":2015,"temrature":0.87},{"Year":2016,"temrature":0.99},{"Year":2017,"temrature":0.91},{"Year":2018,"temrature":0.83}] ; 
+
+
+console.log (data.length) 
+
+// check we have 139 years of data
+var container = document.querySelector('.chart'); 
+
+// start a loop to add one new element per entry 
+data.forEach (function(entry) {
+              
+              // more code here 
+              
+  // elements are alwayes added in two steps
+             var stripe = document.createElement('div'); 
+container.appendChild(stripe);
+stripe.className = 'stripe';
+
+  // now, let's give the elements the right color 
+
+  var color = '';
+
+var colorScale = chroma.scale('RdBu').domain([0.75,-0.75]);
+  color = colorScale(entry.temrature);
+  stripe.style['background-color'] = color; 
+  
+              });
